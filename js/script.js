af45cf40847c2e115926c39880ef9cdca439a747
Jakub Kulikowski <jakubkkulikowski@gmail.com>
2020-04-04 16:09:19 +0200

    Bars JS Works

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const navbar = document.querySelector("#navbar-desktop");
 const navbarRight = document.querySelector("#bottom-side-bar");
 
+const expLeft = document.querySelector(".first-left");
+const expRight = document.querySelector(".first-right");
+
+const bioLeft = document.querySelector(".third-left");
+const bioRight = document.querySelector(".third-right");
+
+const projectLeft = document.querySelector(".second-left");
+const projectRight = document.querySelector(".second-right");
+
+let bounding = projectLeft.getBoundingClientRect();
+
+console.log(bounding);
 
 window.onscroll = function() {
     
@@ -8,10 +20,40 @@ window.onscroll = function() {
         navbar.style.backgroundColor = "#232323";
         navbar.style.boxShadow = "0px 0px 11px 0px rgba(0,0,0,0.75)";
         navbarRight.style.width = "0%";
+
+        if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            expLeft.style.width = "51%";
+            expRight.style.width = "51%";
+
+            if(document.body.scrollTop > 1700 || document.documentElement.scrollTop > 1700) {
+                bioLeft.style.width = "51%";
+                bioRight.style.width = "51%";
+
+                if(document.body.scrollTop > 2300 || document.documentElement.scrollTop > 2300) {
+                    projectLeft.style.width = "51%";
+                    projectRight.style.width = "51%";
+                }
+                else {
+                    projectLeft.style.width = "0";
+                    projectRight.style.width = "0";
+                }
+            }
+            else {
+                bioLeft.style.width = "0";
+                bioRight.style.width = "0";
+            }
+        }
+        else {
+            expLeft.style.width = "0";
+            expRight.style.width = "0";
+        }
+        
     }
+
     else {
         navbar.style.backgroundColor = "transparent";
         navbar.style.boxShadow = "none";
         navbarRight.style.width = "100%";
     }
+    
 }
